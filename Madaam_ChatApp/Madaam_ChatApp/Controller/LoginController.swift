f268f16f44d4da1ac1725777b596a0810561d4a3
IOS DEV <IOS.DEV@mail.com>
2018-09-26 18:03:49 +0800

    corner of bg

--- Madaam_ChatApp/Madaam_ChatApp/Controller/LoginController.swift
+++ Madaam_ChatApp/Madaam_ChatApp/Controller/LoginController.swift
@@ -10,26 +10,31 @@ import UIKit
 
 class LoginController: UIViewController {
     
+    let inputsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 5
+        view.layer.masksToBounds = true
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     //Background Color
     view.backgroundColor = UIColor(r: 61, g: 91, b: 155)
-        
-    //container for  Register Fields
-    let inputsContainerView = UIView()
-        inputsContainerView.backgroundColor = UIColor.white
-        inputsContainerView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(inputsContainerView)
-        
-    //Contraints x, y ,with, height contraints
+    
+    view.addSubview(inputsContainerView)
+    setupInputsContainerView()
+    }
+    func setupInputsContainerView(){
+        //Contraints x, y ,with, height contraints
         inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
         inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
     }
-    
     //Status Bar Style
     func preferredStatusBarStyle() -> UIStatusBarStyle {
     return .lightContent
