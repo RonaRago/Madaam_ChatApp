388167818698a23553151d66fa09556a2b0a3d5c
IOS DEV <IOS.DEV@mail.com>
2018-09-26 14:28:13 +0800

    setup main story board and created a function logout

--- Madaam_ChatApp/Madaam_ChatApp/ViewController.swift
+++ Madaam_ChatApp/Madaam_ChatApp/ViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
     }
 
-
+    @objc func handleLogout(){
+        
+    }
 }
 
