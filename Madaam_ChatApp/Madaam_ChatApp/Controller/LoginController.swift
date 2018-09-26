7c6031991409f702b2fb24ebea17098483e67f22
IOS DEV <IOS.DEV@mail.com>
2018-09-26 18:37:54 +0800

    register button font and size

--- Madaam_ChatApp/Madaam_ChatApp/Controller/LoginController.swift
+++ Madaam_ChatApp/Madaam_ChatApp/Controller/LoginController.swift
@@ -12,36 +12,40 @@ class LoginController: UIViewController {
     
     // Fields Contrainer
     let inputsContainerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor.white
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.layer.cornerRadius = 5
-        view.layer.masksToBounds = true
-        return view
+    let view = UIView()
+    view.backgroundColor = UIColor.white
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.layer.cornerRadius = 5
+    view.layer.masksToBounds = true
+    return view
     }()
     
     // Register Button
     let LoginRegisterButton: UIButton = {
-        let button = UIButton (type: .system)
-        button.backgroundColor = UIColor(r: 80, g: 101, b: 161)
-        button.setTitle("Register", for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitleColor(UIColor.white, for: .normal)
-        return button
+    let button = UIButton (type: .system)
+    button.backgroundColor = UIColor(r: 80, g: 101, b: 161)
+    button.setTitle("Register", for: .normal)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    button.setTitleColor(UIColor.white, for: .normal)
+    button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+    return button
     }()
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+    super.viewDidLoad()
 
     //Background Color
     view.backgroundColor = UIColor(r: 61, g: 91, b: 155)
     
+    //views
     view.addSubview(inputsContainerView)
     view.addSubview(LoginRegisterButton)
     
+    //calls functions
     setupInputsContainerView()
     setupLoginRegisterButton()
     }
+    
     func setupInputsContainerView(){
         //Contraints x, y ,with, height contraints
         inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
@@ -55,8 +59,7 @@ class LoginController: UIViewController {
         LoginRegisterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         LoginRegisterButton.topAnchor.constraint(equalTo: inputsContainerView.bottomAnchor, constant: 12).isActive = true
         LoginRegisterButton.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor, constant: 12).isActive = true
-        LoginRegisterButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        
+        LoginRegisterButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     
     //Status Bar Style
@@ -67,7 +70,7 @@ class LoginController: UIViewController {
 
     //Extension for RGB Colors
     extension UIColor{
-        convenience init(r: CGFloat, g: CGFloat, b: CGFloat){
-        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
-        }
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat){
+    self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
     }
